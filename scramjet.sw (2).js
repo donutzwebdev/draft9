5888ea63a6b41d28ebf626fc01761789d096279d
donutzwebdev <donutz.web.dev@gmail.com>
2026-08-19 19:47:21 -0600

    Add files via upload

---
+++ scramjet.sw (2).js
@@ -0,0 +1,14 @@
+importScripts('https://donutzwebdev.github.io/scramjet/scramjet.all.js');
+
+const { ScramjetServiceWorker } = $scramjetLoadWorker();
+const sw = new ScramjetServiceWorker();
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith((async () => {
+    await sw.loadConfig();
+    if (sw.route(event)) {
+      return sw.fetch(event);
+    }
+    return fetch(event.request);
+  })());
+});
