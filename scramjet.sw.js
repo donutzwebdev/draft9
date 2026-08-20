72093e30d38eeb343eaaf2c95b04ed271918afe2
donutzwebdev <donutzwebdev@gmail.com>
2026-08-20 16:10:00 -0600

    Fix SW only intercept scramjet prefix requests

--- scramjet.sw.js
+++ scramjet.sw.js
@@ -4,6 +4,8 @@ const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const sw = new ScramjetServiceWorker();
 
 self.addEventListener('fetch', (event) => {
+  // Only intercept requests that go through the scramjet prefix
+  if (!event.request.url.includes('/draft9/scramjet/')) return;
   event.respondWith((async () => {
     await sw.loadConfig();
     if (sw.route(event)) {
